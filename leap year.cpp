0df13de57e0e606be906fb65e39c347a9b77e58a
Nithithra14 <120041256+Nithithra14@users.noreply.github.com>
2023-02-09 14:01:17 +0530

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter the year to check if it is a leap year: ");
+    int year;
+	scanf("%d", &year);
+    
+    if(year % 4 == 0){
+        if(year % 100 == 0){  
+            if(year % 400 == 0)
+                printf("\n\n%d is a leap year\n", year);
+            else 
+                printf("\n\n%d is not a leap year\n", year);
+        }
+        else
+            printf("\n\n%d is a leap year\n", year);
+    }
+    else
+        printf("\n\n%d is not a leap year\n", year);
+    return 0;
+}
